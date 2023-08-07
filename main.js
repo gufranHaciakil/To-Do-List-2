433d0472df9d50e9e76bc5f9a5db37df669651f4
gufranHaciakil <gufranhaciakil@gmail.com>
2023-08-08 01:52:23 +0300

    ....

--- main.js
+++ main.js
@@ -5,33 +5,43 @@ const form = document.querySelector("form");
 const mytask = document.getElementById("mytask");
 
 
-// cont.style.border = '2px solid red'
-
 console.log(cont);
 
 cont.addEventListener("click", (event) => {
-  if (event.target.className == "fa-solid fa-trash-can gufi") {
-    event.target.parentElement.parentElement.remove();
-  } else if (event.target.className == "fa-solid fa-face-angry") {
-    event.target.classList.add("dNone");
-    const heart = `<span id="heart"><i id="" class="fa-solid fa-heart"></i></span>`;
-    let finishClass = event.target.parentElement.parentElement.parentElement.getElementsByClassName("finishClass")[0]
-    finishClass.classList.add("finish")
-    event.target.parentElement.parentElement.innerHTML += heart;
+  switch (event.target.className) {
+    case "fa-solid fa-trash-can gufi":
+      event.target.parentElement.parentElement.remove();
+      break;
 
-  } else if (event.target.className == "fa-solid fa-heart") {
-    event.target.parentElement.parentElement.parentElement.getElementsByClassName("finishClass")[0].classList.remove("finish")
-    event.target.classList.add("dNone")
-    let faceAngry = event.target.parentElement.parentElement.parentElement.getElementsByClassName("fa-face-angry")[0]
-    faceAngry.classList.remove("dNone")
+    case "fa-solid fa-face-angry":
+      event.target.classList.add("dNone");
+      const heart = `<span id="heart"><i id="" class="fa-solid fa-heart"></i></span>`;
+      let finishClass = event.target.parentElement.parentElement.parentElement.getElementsByClassName("finishClass")[0]
+      finishClass.classList.add("finish")
+      event.target.parentElement.parentElement.innerHTML += heart;
+      break;
+
+    case "fa-solid fa-heart":
+      event.target.parentElement.parentElement.parentElement.getElementsByClassName("finishClass")[0].classList.remove("finish")
+      event.target.classList.add("dNone")
+      let faceAngry = event.target.parentElement.parentElement.parentElement.getElementsByClassName("fa-face-angry")[0]
+      faceAngry.classList.remove("dNone")
+      break;
+
+    case "fa-solid fa-star":
+      event.target.classList.add("orange")
+      cont.prepend(event.target.parentElement.parentElement)
+      break;
+
+    case "fa-solid fa-star orange":
+      event.target.classList.remove("orange")
+      break;
+
+    default:
+      break;
   }
 
-  else if (event.target.className == "fa-solid fa-star") {
-    event.target.classList.add("orange")
-    cont.prepend(event.target.parentElement.parentElement)
-  } else if (event.target.className == "fa-solid fa-star orange") {
-    event.target.classList.remove("orange")
-  }
+
 });
 
 form.addEventListener("submit", (event) => {
