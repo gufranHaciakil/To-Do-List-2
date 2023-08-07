2e58b682064992787db55eed2ccb22f8f5756c6d
gufranHaciakil <gufranhaciakil@gmail.com>
2023-08-07 19:45:52 +0300

    ..

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const button = document.querySelector("button");
 const input = document.querySelector("input");
 const cont = document.getElementById("cont");
 const form = document.querySelector("form");
+const mytask = document.getElementById("mytask");
+
 
 // cont.style.border = '2px solid red'
 
@@ -9,20 +11,38 @@ console.log(cont);
 
 cont.addEventListener("click", (event) => {
   if (event.target.className == "fa-solid fa-trash-can gufi") {
-    console.log(object);
-  } else {
+    event.target.parentElement.parentElement.remove();
+  } else if (event.target.className == "fa-solid fa-face-angry") {
+    event.target.classList.add("dNone");
+    const heart = `<span id="heart"><i id="" class="fa-solid fa-heart"></i></span>`;
+    let finishClass = event.target.parentElement.parentElement.parentElement.getElementsByClassName("finishClass")[0]
+    finishClass.classList.add("finish")
+    event.target.parentElement.parentElement.innerHTML += heart;
+
+  } else if (event.target.className == "fa-solid fa-heart") {
+    event.target.parentElement.parentElement.parentElement.getElementsByClassName("finishClass")[0].classList.remove("finish")
+    event.target.classList.add("dNone")
+    let faceAngry = event.target.parentElement.parentElement.parentElement.getElementsByClassName("fa-face-angry")[0]
+    faceAngry.classList.remove("dNone")
+  }
+
+  else if (event.target.className == "fa-solid fa-star") {
+    event.target.classList.add("orange")
+    cont.prepend(event.target.parentElement.parentElement)
+  } else if (event.target.className == "fa-solid fa-star orange") {
+    event.target.classList.remove("orange")
   }
 });
 
-form.addEventListener("submit", (eo) => {
-  eo.preventDefault();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
   const taskk = ` 
-        <div class="task">
-            <span><i class="fa-solid fa-star"></i></span>
-                <p>${input.value}</p>
+        <div class="task" id="mytask">
+            <span class="starIcon"><i class="fa-solid fa-star"></i></span>
+                <p class="finishClass">${input.value}</p>
                 <div class="icon">
                     <span class="fa-solid fa-trash-can gufi"></span>
-                    <span><i class="fa-solid fa-face-angry"></i></span>
+                    <span  ><i class="fa-solid fa-face-angry"></i></span>
                 </div>
             </div> 
              
